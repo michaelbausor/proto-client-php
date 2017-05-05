25ae059f2dfb48d29a1a28a1705487f44602843c
Michael Bausor <michaelbausor@google.com>
2017-05-05 14:34:01 -0700

    Add common types

---
+++ src/Google/Api/Servicecontrol/V1/ServiceControllerClient.php
@@ -0,0 +1,87 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+// Original file comments:
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+namespace Google\Api\Servicecontrol\V1 {
+
+  // [Google Service Control API](/service-control/overview)
+  //
+  // Lets clients check and report operations against
+  // a [managed service][google.api.servicemanagement.v1.ManagedService].
+  class ServiceControllerClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+      parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * Checks an operation with Google Service Control to decide whether
+     * the given operation should proceed. It should be called before the
+     * operation is executed.
+     *
+     * If feasible, the client should cache the check results and reuse them for
+     * up to 60s. In case of server errors, the client may rely on the cached
+     * results for longer time.
+     *
+     * This method requires the `servicemanagement.services.check` permission
+     * on the specified service. For more information, see
+     * [Google Cloud IAM](https://cloud.google.com/iam).
+     * @param \Google\Api\Servicecontrol\V1\CheckRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Check(\Google\Api\Servicecontrol\V1\CheckRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicecontrol.v1.ServiceController/Check',
+      $argument,
+      ['\Google\Api\Servicecontrol\V1\CheckResponse', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Reports operations to Google Service Control. It should be called
+     * after the operation is completed.
+     *
+     * If feasible, the client should aggregate reporting data for up to 5s to
+     * reduce API traffic. Limiting aggregation to 5s is to reduce data loss
+     * during client crashes. Clients should carefully choose the aggregation
+     * window to avoid data loss risk more than 0.01% for business and
+     * compliance reasons.
+     *
+     * This method requires the `servicemanagement.services.report` permission
+     * on the specified service. For more information, see
+     * [Google Cloud IAM](https://cloud.google.com/iam).
+     * @param \Google\Api\Servicecontrol\V1\ReportRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Report(\Google\Api\Servicecontrol\V1\ReportRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicecontrol.v1.ServiceController/Report',
+      $argument,
+      ['\Google\Api\Servicecontrol\V1\ReportResponse', 'decode'],
+      $metadata, $options);
+    }
+
+  }
+
+}
---
+++ src/Google/Api/Servicemanagement/V1/ServiceManagerClient.php
@@ -0,0 +1,297 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+// Original file comments:
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+namespace Google\Api\Servicemanagement\V1 {
+
+  // [Google Service Management API](/service-management/overview)
+  class ServiceManagerClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+      parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * Lists all managed services.
+     * @param \Google\Api\Servicemanagement\V1\ListServicesRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListServices(\Google\Api\Servicemanagement\V1\ListServicesRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/ListServices',
+      $argument,
+      ['\Google\Api\Servicemanagement\V1\ListServicesResponse', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Gets a managed service.
+     * @param \Google\Api\Servicemanagement\V1\GetServiceRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetService(\Google\Api\Servicemanagement\V1\GetServiceRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/GetService',
+      $argument,
+      ['\Google\Api\Servicemanagement\V1\ManagedService', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Creates a new managed service.
+     * Please note one producer project can own no more than 20 services.
+     *
+     * Operation<response: ManagedService>
+     * @param \Google\Api\Servicemanagement\V1\CreateServiceRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateService(\Google\Api\Servicemanagement\V1\CreateServiceRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/CreateService',
+      $argument,
+      ['\Google\Longrunning\Operation', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Deletes a managed service. This method will change the serivce in the
+     * `Soft-Delete` state for 30 days. Within this period, service producers may
+     * call [UndeleteService][google.api.servicemanagement.v1.ServiceManager.UndeleteService] to restore the service.
+     * After 30 days, the service will be permanently deleted.
+     *
+     * Operation<response: google.protobuf.Empty>
+     * @param \Google\Api\Servicemanagement\V1\DeleteServiceRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function DeleteService(\Google\Api\Servicemanagement\V1\DeleteServiceRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/DeleteService',
+      $argument,
+      ['\Google\Longrunning\Operation', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Revives a previously deleted managed service. The method restores the
+     * service using the configuration at the time the service was deleted.
+     * The target service must exist and must have been deleted within the
+     * last 30 days.
+     *
+     * Operation<response: UndeleteServiceResponse>
+     * @param \Google\Api\Servicemanagement\V1\UndeleteServiceRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function UndeleteService(\Google\Api\Servicemanagement\V1\UndeleteServiceRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/UndeleteService',
+      $argument,
+      ['\Google\Longrunning\Operation', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Lists the history of the service configuration for a managed service,
+     * from the newest to the oldest.
+     * @param \Google\Api\Servicemanagement\V1\ListServiceConfigsRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListServiceConfigs(\Google\Api\Servicemanagement\V1\ListServiceConfigsRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/ListServiceConfigs',
+      $argument,
+      ['\Google\Api\Servicemanagement\V1\ListServiceConfigsResponse', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Gets a service configuration (version) for a managed service.
+     * @param \Google\Api\Servicemanagement\V1\GetServiceConfigRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetServiceConfig(\Google\Api\Servicemanagement\V1\GetServiceConfigRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/GetServiceConfig',
+      $argument,
+      ['\Google\Api\Service', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Creates a new service configuration (version) for a managed service.
+     * This method only stores the service configuration. To roll out the service
+     * configuration to backend systems please call
+     * [CreateServiceRollout][google.api.servicemanagement.v1.ServiceManager.CreateServiceRollout].
+     * @param \Google\Api\Servicemanagement\V1\CreateServiceConfigRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateServiceConfig(\Google\Api\Servicemanagement\V1\CreateServiceConfigRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/CreateServiceConfig',
+      $argument,
+      ['\Google\Api\Service', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Creates a new service configuration (version) for a managed service based
+     * on
+     * user-supplied configuration source files (for example: OpenAPI
+     * Specification). This method stores the source configurations as well as the
+     * generated service configuration. To rollout the service configuration to
+     * other services,
+     * please call [CreateServiceRollout][google.api.servicemanagement.v1.ServiceManager.CreateServiceRollout].
+     *
+     * Operation<response: SubmitConfigSourceResponse>
+     * @param \Google\Api\Servicemanagement\V1\SubmitConfigSourceRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function SubmitConfigSource(\Google\Api\Servicemanagement\V1\SubmitConfigSourceRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/SubmitConfigSource',
+      $argument,
+      ['\Google\Longrunning\Operation', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Lists the history of the service configuration rollouts for a managed
+     * service, from the newest to the oldest.
+     * @param \Google\Api\Servicemanagement\V1\ListServiceRolloutsRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListServiceRollouts(\Google\Api\Servicemanagement\V1\ListServiceRolloutsRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/ListServiceRollouts',
+      $argument,
+      ['\Google\Api\Servicemanagement\V1\ListServiceRolloutsResponse', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Gets a service configuration [rollout][google.api.servicemanagement.v1.Rollout].
+     * @param \Google\Api\Servicemanagement\V1\GetServiceRolloutRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetServiceRollout(\Google\Api\Servicemanagement\V1\GetServiceRolloutRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/GetServiceRollout',
+      $argument,
+      ['\Google\Api\Servicemanagement\V1\Rollout', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Creates a new service configuration rollout. Based on rollout, the
+     * Google Service Management will roll out the service configurations to
+     * different backend services. For example, the logging configuration will be
+     * pushed to Google Cloud Logging.
+     *
+     * Please note that any previous pending and running Rollouts and associated
+     * Operations will be automatically cancelled so that the latest Rollout will
+     * not be blocked by previous Rollouts.
+     *
+     * Operation<response: Rollout>
+     * @param \Google\Api\Servicemanagement\V1\CreateServiceRolloutRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateServiceRollout(\Google\Api\Servicemanagement\V1\CreateServiceRolloutRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/CreateServiceRollout',
+      $argument,
+      ['\Google\Longrunning\Operation', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Generates and returns a report (errors, warnings and changes from
+     * existing configurations) associated with
+     * GenerateConfigReportRequest.new_value
+     *
+     * If GenerateConfigReportRequest.old_value is specified,
+     * GenerateConfigReportRequest will contain a single ChangeReport based on the
+     * comparison between GenerateConfigReportRequest.new_value and
+     * GenerateConfigReportRequest.old_value.
+     * If GenerateConfigReportRequest.old_value is not specified, this method
+     * will compare GenerateConfigReportRequest.new_value with the last pushed
+     * service configuration.
+     * @param \Google\Api\Servicemanagement\V1\GenerateConfigReportRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GenerateConfigReport(\Google\Api\Servicemanagement\V1\GenerateConfigReportRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/GenerateConfigReport',
+      $argument,
+      ['\Google\Api\Servicemanagement\V1\GenerateConfigReportResponse', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Enable a managed service for a project with default setting.
+     *
+     * Operation<response: EnableServiceResponse>
+     *
+     * [google.rpc.Status][google.rpc.Status] errors may contain a
+     * [google.rpc.PreconditionFailure][] error detail.
+     * @param \Google\Api\Servicemanagement\V1\EnableServiceRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function EnableService(\Google\Api\Servicemanagement\V1\EnableServiceRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/EnableService',
+      $argument,
+      ['\Google\Longrunning\Operation', 'decode'],
+      $metadata, $options);
+    }
+
+    /**
+     * Disable a managed service for a project.
+     *
+     * Operation<response: DisableServiceResponse>
+     * @param \Google\Api\Servicemanagement\V1\DisableServiceRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function DisableService(\Google\Api\Servicemanagement\V1\DisableServiceRequest $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/google.api.servicemanagement.v1.ServiceManager/DisableService',
+      $argument,
+      ['\Google\Longrunning\Operation', 'decode'],
+      $metadata, $options);
+    }
+
+  }
+
+}
